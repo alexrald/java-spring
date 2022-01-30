b280464b6b3f84b83867f44375dc3f85550453f2
alexrald <alexral.ee@gmail.com>
2022-01-30 12:37:05 +0200

    [change] - Update

--- src/main/java/com/sda/javaspring/component/hierarchy/MovieRepository.java
+++ src/main/java/com/sda/javaspring/component/hierarchy/MovieRepository.java
@@ -2,6 +2,12 @@ package com.sda.javaspring.component.hierarchy;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class MovieRepository {
+
+    public List<Movie> readAllMoviesFromDatabase() {
+        return List.of();
+    }
 }
