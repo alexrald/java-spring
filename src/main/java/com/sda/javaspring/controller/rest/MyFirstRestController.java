5d8f2f33a22c13fdeb09b9b3ef8fe15ea8450a5d
alexrald <alexral.ee@gmail.com>
2022-02-12 11:22:52 +0200

    [change] - json collection endpoint for persons

--- src/main/java/com/sda/javaspring/controller/rest/MyFirstRestController.java
+++ src/main/java/com/sda/javaspring/controller/rest/MyFirstRestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @Slf4j
 @RequestMapping("/rest")
@@ -15,6 +17,22 @@ public class MyFirstRestController {
     @GetMapping("/one-person")
     public Person onePerson() {
         return new Person("Alexander", "Raldugin", 29);
+    }
+
+    @GetMapping("/persons")
+    public List<Person> persons() {
+        return List.of(
+                new Person("John", "Doe", 99),
+                new Person("Jane", "Roe", 33)
+        );
+    }
+
+    @GetMapping("/persons-array")
+    public Person[] personsArray() {
+        return new Person[] {
+                new Person("John", "Doe", 99),
+                new Person("Jane", "Roe", 33)
+        };
     }
 
 }
