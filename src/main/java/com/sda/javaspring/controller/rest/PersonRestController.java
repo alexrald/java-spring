3a4931ef35075aed13ad41e03c1a7f1f86808c5c
alexrald <alexral.ee@gmail.com>
2022-02-13 13:45:59 +0200

    [add] - script to initialize database with one endpoint for reading all PersonEntity elemets

--- src/main/java/com/sda/javaspring/controller/rest/PersonRestController.java
+++ src/main/java/com/sda/javaspring/controller/rest/PersonRestController.java
@@ -1,18 +1,32 @@
 package com.sda.javaspring.controller.rest;
 
+import com.sda.javaspring.entity.PersonEntity;
 import com.sda.javaspring.service.PersonService;
+import com.sda.javaspring.service.RealPersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
+@RequestMapping("/api")
 public class PersonRestController {
 
-    private final PersonService personService;
+    private final RealPersonService personService;
 
     @Autowired
-    public PersonRestController(PersonService personService) {
+    public PersonRestController(RealPersonService personService) {
         this.personService = personService;
+    }
+
+    @GetMapping("/persons")
+    public List<PersonEntity> findAllPersons() {
+        log.info("findAllPersons()");
+
+        return personService.readAllPersonEntities();
     }
 }
--- src/main/java/com/sda/javaspring/service/RealPersonService.java
+++ src/main/java/com/sda/javaspring/service/RealPersonService.java
@@ -1,8 +1,11 @@
 package com.sda.javaspring.service;
 
+import com.sda.javaspring.entity.PersonEntity;
 import com.sda.javaspring.repository.PersonRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -12,5 +15,14 @@ public class RealPersonService {
 
     public RealPersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
+    }
+
+    public List<PersonEntity> readAllPersonEntities() {
+        log.info("Trying to read all persons entities");
+
+        var result = personRepository.findAll();
+        log.info("persons entities read from db: {}", result);
+
+        return result;
     }
 }
